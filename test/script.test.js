ffa99e49f5aaee27f77a11e93457845d474e2927
JAGGANRAJ27 <njwebdesigning@gmail.com>
2025-02-01 09:20:16 +0530

    remove comments in script.test.js

--- test/script.test.js
+++ test/script.test.js
@@ -1,15 +1,10 @@
-// Import the function to be tested
 const showMessage = require('../script');
 
-// Jest setup: reset the DOM before each test
 beforeEach(() => {
-  // Set up a simple DOM structure
   document.body.innerHTML = '<div><p id="message"></p></div>';
 });
 
 test('should update message in the DOM', () => {
   showMessage();
-
-  // Assert that the message in the DOM is updated correctly
   expect(document.getElementById("message").innerText).toBe("Hello, Jenkins is working!");
 });
